6f22d4f1940a3fcb0ad4e872fdeb25222ed93368
xmliu <xiaominliu@yufuid.com>
2021-07-20 17:41:11 +0800

    feat(core): [Input] fix limit len calculate func
    affects: @acme-ui/core

--- packages/core/src/components/Input/Input.tsx
+++ packages/core/src/components/Input/Input.tsx
@@ -152,18 +152,21 @@ const Input: React.ForwardRefExoticComponent<InputProps & React.RefAttributes<HT
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       const internalValue = get(e, 'target.value');
-      const valueLength = isString(internalValue) ? internalValue.length : 0;
       if (!('value' in props)) {
         setCurrentValue(internalValue);
-      }
-      if (limit) {
-        setCurrentValueLen(valueLength);
-        setLimitError(valueLength > limit);
       }
       if (isFunction(onChange)) {
         onChange(e);
       }
     };
+
+    React.useEffect(() => {
+      const valueLength = isString(currentValue) ? currentValue.length : 0;
+      setCurrentValueLen(valueLength);
+      if (limit) {
+        setLimitError(valueLength > limit);
+      }
+    }, [currentValue, limit]);
 
     const [isFocus, setIsFocus] = React.useState(false);
     const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
